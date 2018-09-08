79c8fbc700c26f77dc781c386f1fb608c12f1a3d
zhs007 <sssxueren@gmail.com>
2018-09-08 11:14:42 +0800

    upd

--- startdocker.sh
+++ startdocker.sh
@@ -1,3 +1,3 @@
 docker container stop jarvissh
 docker container rm jarvissh
-docker run -d -p 7788:7788 --name jarvissh -v $PWD/data:/home/jarvissh/data jarvissh
+docker run -d -p 7788:7788 --name jarvissh -v $PWD/runpath:/home/jarvissh/data jarvissh
