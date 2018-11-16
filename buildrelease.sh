7be0a59e7bcc4932ae0999d4a51b2bad1e2636e9
zhs007 <sssxueren@gmail.com>
2018-11-16 16:51:46 +0800

    upd

--- buildrelease.sh
+++ buildrelease.sh
@@ -11,7 +11,7 @@ docker run -d \
 rm -rf $PWD/release/jarvissh
 docker container cp jarvissh_buildrelease:/home/jarvissh $PWD/release/jarvissh
 rm -rf $PWD/release/jarvissh/dat/coredb
-tar zcvf $PWD/release/jarvissh.tar.gz $PWD/release/jarvissh
+tar zcvf $PWD/release/jarvissh.tar.gz release/jarvissh
 
 docker container stop jarvissh_buildrelease
 docker container rm jarvissh_buildrelease
