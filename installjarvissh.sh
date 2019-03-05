235e8b2cf2e74f5baa9943252322d929512fa8c9
zhs007 <sssxueren@gmail.com>
2019-03-05 14:18:43 +0800

    - updated scripts

--- installjarvissh.sh
+++ installjarvissh.sh
@@ -2,5 +2,4 @@ rm -rf jarvissh.tar.gz
 wget https://github.com/zhs007/jarvissh/releases/download/v0.2.5/jarvissh.tar.gz
 tar zxvf jarvissh.tar.gz
 cd jarvissh
-./jarvissh stop
-./jarvissh start -d
+./jarvissh restart -d
