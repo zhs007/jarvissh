bcf3c054098821d5e5abf994b72891e891af3a11
zhs007 <sssxueren@gmail.com>
2019-04-01 09:24:43 +0800

    updated jarviscore to v0.7.120

--- installjarvissh.sh
+++ installjarvissh.sh
@@ -1,5 +1,5 @@
 rm -rf jarvissh.tar.gz
-wget https://github.com/zhs007/jarvissh/releases/download/v0.2.5/jarvissh.tar.gz
+wget https://github.com/zhs007/jarvissh/releases/download/v0.2.45/jarvissh.tar.gz
 tar zxvf jarvissh.tar.gz
 cd jarvissh
 ./jarvissh restart -d
