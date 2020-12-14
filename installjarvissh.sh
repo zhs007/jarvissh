8762d6a33912d54ea6dc0afafbaafdb216e313d4
Zerro Zhao <sssxueren@gmail.com>
2020-12-14 13:47:48 +0800

    Update installjarvissh.sh

--- installjarvissh.sh
+++ installjarvissh.sh
@@ -1,5 +1,7 @@
 rm -rf jarvissh.tar.gz
 wget https://github.com/zhs007/jarvissh/releases/download/v0.2.92/jarvissh.linux64.tar.gz
+mv jarvissh.linux64.tar.gz jarvissh.tar.gz
 tar zxvf jarvissh.tar.gz
+mv jarvissh.linux64 jarvissh
 cd jarvissh
 ./jarvissh restart -d
